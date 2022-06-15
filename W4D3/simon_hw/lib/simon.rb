3b5d0f9a22601b43fa48156b04d8ce05f46ea2cc
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-14 20:37:24 -0400

    finished W4D3 hw

--- W4D3/simon_hw/lib/simon.rb
+++ W4D3/simon_hw/lib/simon.rb
@@ -4,38 +4,65 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
 
   end
 
   def play
-
+    until game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 0.75
+      system("clear")
+      sleep 0.25
+    end
   end
 
   def require_sequence
-
+    p "Please repeat back the sequence of colors, each separated by a space."
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color != user_color
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
-
+    seq << COLORS.sample
   end
 
   def round_success_message
-
+    p "Nice job! Next round coming up."
   end
 
   def game_over_message
-
+    p "WRONG! GAME OVER!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
