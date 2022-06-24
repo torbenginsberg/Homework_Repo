cc1fa153116ebfcf589f2bfd228cc27ceb89639b
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-24 10:39:23 -0400

    more updates

--- W5D5/movie_buff/movie_novice/queries.rb
+++ W5D5/movie_buff/movie_novice/queries.rb
@@ -23,15 +23,15 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
-  Actor.find_by(name: 'Angeline Jolie')
+  Actor.find_by(name: 'Angelina Jolie')
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
   Movie
-    .select(:title)
-    .where(':score >= 9')
+    .select(:id, :title)
+    .where('score >= 9')
 end
 
 def star_wars
@@ -39,7 +39,7 @@ def star_wars
   # hint: use 'select' and 'where'
   Movie
     .select(:id, :title, :yr)
-    .where(:title LIKE '%Star Wars%')
+    .where('title LIKE \'%Star Wars%\'')
 end
 
 
@@ -49,7 +49,7 @@ def below_average_years
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
   Movie
-    .select(:yr, :COUNT(*) as bad_movies)
+    .select('yr', 'COUNT(*) as bad_movies')
     .where('score < 5')
     .group(:yr)
     .order('bad_movies DESC')
@@ -61,14 +61,19 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-  
+  Actor
+    .order(:name)
+    .limit(10)
 end
 
 def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where('title = \'Pulp Fiction\'')
 end
 
 def uma_movies
@@ -76,5 +81,8 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie
+    .select(:id, :title, :yr)
+    .joins(:actors)
+    .where('name = \'Uma Thurman\'')
 end
