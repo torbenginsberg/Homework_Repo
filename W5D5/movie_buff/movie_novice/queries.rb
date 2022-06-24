fbf57a06edf46db92f69e02e357cf7a606e163a1
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-24 10:14:05 -0400

    W5D5 hw updates

--- W5D5/movie_buff/movie_novice/queries.rb
+++ W5D5/movie_buff/movie_novice/queries.rb
@@ -23,19 +23,23 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
-
+  Actor.find_by(name: 'Angeline Jolie')
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-
+  Movie
+    .select(:title)
+    .where(':score >= 9')
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
-
+  Movie
+    .select(:id, :title, :yr)
+    .where(:title LIKE '%Star Wars%')
 end
 
 
@@ -44,7 +48,11 @@ def below_average_years
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
   # hint: use 'select', 'where', 'group', 'order'
-
+  Movie
+    .select(:yr, :COUNT(*) as bad_movies)
+    .where('score < 5')
+    .group(:yr)
+    .order('bad_movies DESC')
 end
 
 def alphabetized_actors
@@ -53,7 +61,7 @@ def alphabetized_actors
   # Note: Ubuntu users may find that special characters
   # are alphabetized differently than the specs.
   # This spec might fail for Ubuntu users. It's ok!
-
+  
 end
 
 def pulp_fiction_actors
